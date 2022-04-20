c8e22b757e523f290ddd199a500d9f4558cfdf36
yongjae_park <ytothej92@gmail.com>
2022-04-20 13:26:51 +0900

    refactor(RandomNumber) : 랜덤 값 list로 반환하도록 수정

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,12 +1,22 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RandomNumber {
     private final static int MIN_NUMBER = 0;
     private final static int MAX_NUMBER = 9;
+    private final static List<Integer> randomNumbers = new ArrayList<>();
 
-    public static int getRandomNumber() {
+    public static List<Integer> generateRandomNumbers(int carsSize) {
+        for (int i = 0 ; i < carsSize ; i++) {
+            randomNumbers.add(getRandomNumber());
+        }
+        return randomNumbers;
+    }
+
+    private static int getRandomNumber() {
         int number = Randoms.pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
         return number;
     }
--- src/test/java/racingcar/domain/RandomNumberTest.java
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,7 +14,15 @@ public class RandomNumberTest {
     @Test
     @DisplayName("0-9사이_랜덤값_생성_테스트")
     void 랜덤값_범위내_생성_테스트() {
-        int randomNumber = RandomNumber.getRandomNumber();
+        int randomNumber = Randoms.pickNumberInRange(0,9);
         assertThat(String.valueOf(randomNumber).matches(RANDOM_REGEX)).isTrue();
+    }
+
+    @Test
+    @DisplayName("참여자_수만큼_랜덤넘버_생성_테스트")
+    void 참여자_수만큼_랜덤넘버_생성_테스트() {
+        int carsSize = 5;
+        List<Integer> randomNumbers = RandomNumber.generateRandomNumbers(carsSize);
+        assertThat(randomNumbers.size()).isEqualTo(carsSize);
     }
 }
