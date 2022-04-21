83f5f16bfd784849898d81b2058f3069290c58b0
yongjae_park <ytothej92@gmail.com>
2022-04-21 16:27:08 +0900

    feat(Referee) : (단일 우승의 경우) 우승자 체크 추가

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -1,8 +1,26 @@
 package racingcar.domain;
 
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+
+import java.util.Collections;
+import java.util.List;
+
 public class Referee {
 
-    public GoOrStop turnOver(int randomNumber, Car car) {
+    private Cars cars;
+
+    public Referee(Cars cars) {
+        this.cars = cars;
+    }
+
+    public void turnOver(List<Car> cars, List<Integer> randomNumbers) {
+        for (int i = 0; i < cars.size(); i++) {
+            turnOver(cars.get(i), randomNumbers.get(i));
+        }
+    }
+
+    public GoOrStop turnOver(Car car, int randomNumber) {
         if (randomNumber > 3) {
             moving(GoOrStop.GO, car);
             return GoOrStop.GO;
@@ -12,8 +30,19 @@ public class Referee {
 
     private void moving(GoOrStop goOrStop, Car car) {
         if (goOrStop.isGo())
-            car.goForward();
+            car.moveForward();
     }
 
+    public String whoWin() {
+        List<Car> sortedResult = sort();
+        //TODO : 공동우승
+        return sortedResult.get(0).getCarName().getName();
+    }
+
+    public List<Car> sort() {
+        Collections.sort(cars.getCars(), (firstOne, secondOne)
+                -> secondOne.getCarPosition().getPosition() - firstOne.getCarPosition().getPosition());
+        return cars.getCars();
+    }
 
 }
--- src/test/java/racingcar/domain/RefereeTest.java
+++ src/test/java/racingcar/domain/RefereeTest.java
@@ -3,6 +3,12 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.CarGenerator;
+import racingcar.domain.car.Cars;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,10 +16,12 @@ public class RefereeTest {
     private Referee referee;
     private final int GO_RANGE_NUMBER = 5;
     private final int STOP_RANGE_NUMBER = 2;
+    private Cars cars;
 
     @BeforeEach
     void setUp() {
-        referee = new Referee();
+        cars = new Cars(CarGenerator.registerCars("Park,Yong,Jun"));
+        referee = new Referee(cars);
     }
 
     @Test
@@ -21,8 +29,8 @@ public class RefereeTest {
     void goForward() {
         int randomNumber = GO_RANGE_NUMBER;
         Car car = new Car("Jin");
-        assertThat(referee.turnOver(randomNumber, car)).isEqualTo(GoOrStop.GO);
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(referee.turnOver(car, randomNumber)).isEqualTo(GoOrStop.GO);
+        assertThat(car.getCarPosition()).isEqualTo(1);
     }
 
     @Test
@@ -30,8 +38,21 @@ public class RefereeTest {
     void stop() {
         int randomNumber = STOP_RANGE_NUMBER;
         Car car = new Car("Park");
-        assertThat(referee.turnOver(randomNumber, car)).isEqualTo(GoOrStop.STOP);
-        assertThat(car.getPosition()).isEqualTo(0);
+        assertThat(referee.turnOver(car, randomNumber)).isEqualTo(GoOrStop.STOP);
+        assertThat(car.getCarPosition()).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("한번의턴_단일_우승_테스트")
+    void turnOverTest() {
+        List<Integer> randomNumbers = Arrays.asList(2, 3, 4);
+        referee.turnOver(cars.getCars(), randomNumbers);
+        assertThat(referee.whoWin()).isEqualTo("Jun");
+    }
+
+    @Test
+    @DisplayName("한번의턴_공동_우승_테스트")
+    void turnOverTest_공동우승() {
+       //TODO : 공동우승 테스트
+    }
 }
