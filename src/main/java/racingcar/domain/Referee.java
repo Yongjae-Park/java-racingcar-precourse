b90e280b7f28014cdf3599ad41fb5def32cc5356
yongjae_park <ytothej92@gmail.com>
2022-04-23 23:19:54 +0900

    feat(Referee) : 공동우승 판단 로직 추가

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -4,6 +4,7 @@ import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Referee {
@@ -17,6 +18,7 @@ public class Referee {
         this.winNames = new WinNames(new ArrayList<>());
         this.result = new Result(winNames);
     }
+
     public void turnOver(List<Car> cars) {
         for (int i = 0; i < cars.size(); i++) {
             turnOver(cars.get(i), RandomNumber.generateRandomNumber());
@@ -32,5 +34,28 @@ public class Referee {
     private void moving(GoOrStop goOrStop, Car car) {
         if (goOrStop.isGo())
             car.moveForward();
+    }
+
+    public String whoWin() {
+        return getResult(sort()).getWinners();
+    }
+
+    private List<Car> sort() {
+        Collections.sort(cars.getCars(), (firstOne, secondOne)
+                -> secondOne.getCarPosition().getPosition() - firstOne.getCarPosition().getPosition());
+        return cars.getCars();
+    }
+
+    private Result getResult(List<Car> sortedResult) {
+        sortedResult.forEach(car -> {
+            if (car.getCarPosition().getPosition() == getHighestPosition(sortedResult)) {
+                result.addWinner(car.getCarName());
+            }
+        });
+        return result;
+    }
+
+    private int getHighestPosition(List<Car> sortedResult) {
+        return sortedResult.get(0).getCarPosition().getPosition();
     }
 }
