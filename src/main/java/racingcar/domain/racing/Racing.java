e3e3983d3271d5cc874568a44f9a90c05509e36d
yongjae_park <ytothej92@gmail.com>
2022-04-24 14:14:19 +0900

    feat(Racing) : 레이싱 게임 진행을 담당하는 Racing 클래스 추가

---
+++ src/main/java/racingcar/domain/racing/Racing.java
@@ -0,0 +1,47 @@
+package racingcar.domain.racing;
+
+import racingcar.console.Output;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
+
+public class Racing {
+    private Cars cars;
+    private Turn turn;
+    private Referee referee;
+
+    public Racing(Cars cars, Referee referee, Turn turn) {
+        this.cars = cars;
+        this.referee = referee;
+        this.turn = turn;
+    }
+
+    public void start() {
+        while (!turn.isOver()) {
+            play();
+            turn.increaseCurrentTurn();
+            printTurnOverResult();
+        }
+        String winners = referee.whoWin();
+        Output.printMessage(Message.WINNER_MESSAGE.getMessage() + winners);
+    }
+
+    public void play() {
+        referee.turnOver(cars.getCars());
+    }
+
+    public void printTurnOverResult() {
+        Output.printMessage(Message.TURN_OVER_RESULT_MESSAGE.getMessage());
+        for (Car car : cars.getCars()) {
+            Output.printMessage(car.getCarName().getName() + " : " +
+                    getPositionBar(car.getCarPosition().getPosition()));
+        }
+    }
+
+    public String getPositionBar(int position) {
+        String result = "";
+        for (int i = 0; i < position; i++) {
+            result = result.concat("-");
+        }
+        return result;
+    }
+}
